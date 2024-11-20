63c0690349e54b68e9b07905d1ef71eb3a3d8a97
jixASU <jixcoy@asu.edu>
2024-11-20 10:00:01 -0700

    Refactor keyboardControlsV3.m

--- keyboardControlsV3.m
+++ keyboardControlsV3.m
@@ -18,13 +18,12 @@ threshold = 82;
 %list of cors with their associated number
 colorDestinationArr = ["0" "1" "2" "3" "4" "5" "6" "7"; "NaN" "Black" "Blue" "Green" "Yellow" "Red" "White" "Brown"];
 
-
 %check of global variable for starting destination exists
 if (~exist('colorStart', 'var')) %if not, make it
     global colorStart 
     colorStart = "green";
 elseif isempty(colorStart) % if empty, give it something
-    colorStart = "green"
+    colorStart = "green";
 end
 for i = 1 : 1 : 8 %You know, that color array up there that seemed useless. Well now we itterate through it
     colorz = upper(colorDestinationArr(2,i)); %value extracted from array
@@ -33,8 +32,6 @@ for i = 1 : 1 : 8 %You know, that color array up there that seemed useless. Well
         colorStartInt = str2double(colorDestinationArr(1,i)); %it does? then feed our colorStartInt with the color integer
     end
 end
-
-
 
 if (~exist('colorDestination', 'var'))
     global colorDestination 
@@ -50,62 +47,104 @@ for i = 1 : 1 : 8
     end
 end
 
+% Left Motor, Right Motor, Both Motor, and Scooper global variables, in case of port switch
+global LM
+global RM
+global BM
+global S
+LM = 'C'
+RM = 'A'
+BM = 'AC'
+S = 'D'
 
-% While loop for going to destination
-
-
-
-% While loop for dropping off
+% MAIN LOOP
 while (~stopCar)
     % Move forward
-    brick.MoveMotor('AC', -50);
+    brick.MoveMotor(BM, -50);
     %brick.beep();
     % Save the color it sees
     color = brick.ColorCode(4);
     
     if (color == 5) %If it is red it will stop
-        brick.StopMotor('AC')
+        brick.StopMotor(BM)
         pause(1)
-        
-    end
+    end 
+    else if ( color == 3 | color == 4 )
+        % Color is green or blue
+
+        % TODO: SWITCH TO MANUAL CONTROL FOR PICKUP
+        while (1)
+            pause(0.1)
+            switch key
+                case 'uparrow'
+                    brick.MoveMotor(BM, -50);
+                case 'downarrow'
+                    brick.MoveMotor(BM, 50);
+                case 'leftarrow'
+                    brick.MoveMotor(RM, -25);
+                    brick.MoveMotor(LM, 25);
+                case 'rightarrow'
+                    brick.MoveMotor(RM, 25);
+                    brick.MoveMotor(BM, -25);
+                case 'w'
+                    brick.MoveMotor(S, 5);
+                case 'd'
+                    brick.MoveMotor(S, 10);
+                case 'a'
+                    brick.MoveMotor(S, -10);
+                case 's'
+                    brick.MoveMotor(S, -5);
+                
+                case 'e'
+                    stopCar = false;
+                    disp('Manual Control Ended');
+                    break;
+                case 0
+                    brick.StopMotor('D');
+                    brick.StopMotor(BM);
+                    
+            end
+        end
 
     % MAKE CHANGES TO DISTANCE SENSOR
     distance = brick.UltrasonicDist(2);
-    
+
+    % TURN RIGHT
     if (distance > threshold)
-        brick.MoveMotor('AC', 0);
+        brick.MoveMotor(BM, 0);
         pause(.5) %stop motor and pause for .5 seconds
 
         %if we see gap, move forward a litte bit
-        brick.MoveMotor('AC', -50);
+        brick.MoveMotor(BM, -50);
         pause(1.25)%time increased form 1 to 1.25 to actually allow bot to turn
                         %without this, bot gets caught in walls while turning
         distance = brick.UltrasonicDist(2);
 
         %if we still se a gap, actually turn
         if (distance > threshold)
-            brick.MoveMotor('C', -50);
+            brick.MoveMotor(LM, -50);
             pause(.25)
             % Turns, stops, then goes straight a bit
-            brick.MoveMotor('A', 50);
-            brick.MoveMotor('C', -50);
+            brick.MoveMotor(RM, 50);
+            brick.MoveMotor(LM, -50);
             pause(.85); 
-            brick.MoveMotor('AC', -20);
+            brick.MoveMotor(BM, -20);
         end
-      
+    % ADJUST
     elseif distance < 5
-        brick.MoveMotor('A', -50);
-            brick.MoveMotor('C', -45);
+        brick.MoveMotor(LM, -50);
+            brick.MoveMotor(RM, -45);
             pause(.5);
     end
 
-    if (brick.TouchPressed(1) == 1) % Will turn left if the button is pressed
-        brick.MoveMotor('AC', 0);
+    % BUTTON PRESSED TURN RIGHT
+    if (brick.TouchPressed(1) == 1)
+        brick.MoveMotor(BM, 0);
         pause(.5)
-        brick.MoveMotor('AC', 50);
+        brick.MoveMotor(BM, 50);
         pause(.5)
-        brick.MoveMotor('A', -50);
-        brick.MoveMotor('C', 50);
+        brick.MoveMotor(RM, -50);
+        brick.MoveMotor(CM, 50);
         pause(.85)
     end
 end
@@ -114,43 +153,34 @@ while (1)
     pause(0.1)
     switch key
         case 'uparrow'
-            disp('forward');
-            brick.MoveMotor('AC', -50);
+            brick.MoveMotor(BM, -50);
         case 'downarrow'
-            disp('backward');
-            brick.MoveMotor('AC', 50);
+            brick.MoveMotor(BM, 50);
         case 'leftarrow'
-            disp('left turn');
-            brick.MoveMotor('A', -25);
-            brick.MoveMotor('C', 25);
+            brick.MoveMotor(RM, -25);
+            brick.MoveMotor(LM, 25);
         case 'rightarrow'
-            disp('right turn');
-            brick.MoveMotor('A', 25);
-            brick.MoveMotor('C', -25);
+            brick.MoveMotor(RM, 25);
+            brick.MoveMotor(BM, -25);
         case 'w'
-            disp('scopper up');
-            brick.MoveMotor('D', 5);
+            brick.MoveMotor(S, 5);
         case 'd'
-            disp('scopper up');
-            brick.MoveMotor('D', 10);
+            brick.MoveMotor(S, 10);
         case 'a'
-            disp('scooper down');
-            brick.MoveMotor('D', -10);
+            brick.MoveMotor(S, -10);
         case 's'
-            disp('scooper down');
-            brick.MoveMotor('D', -5);
+            brick.MoveMotor(S, -5);
         
         case 'e'
             stopCar = false;
-            disp('break');
+            disp('Manual Control Ended');
             break;
         case 0
-            %disp('not moving')
             brick.StopMotor('D');
-            brick.StopMotor('AC');
+            brick.StopMotor(BM);
             
     end
 end
 brick.StopMotor('D');
-brick.StopMotor('AC');
+brick.StopMotor(BM);
 CloseKeyboard();
